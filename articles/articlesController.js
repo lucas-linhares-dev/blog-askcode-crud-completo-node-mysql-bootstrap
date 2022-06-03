c4c7664f820b961b6fcf0cdea3dd0f4b683af513
Lucas <lucas_linhares_@hotmail.com>
2022-06-03 17:40:35 -0300

    Funcionalidades e front end do usuário + ediçao e deleçao de artigos + front end home + paginação

--- articles/articlesController.js
+++ articles/articlesController.js
@@ -4,6 +4,7 @@ const Article = require("../articles/Article");
 const Category = require("../categories/Category")
 const slugify = require("slugify");
 
+
 router.get("/admin/articles",(req,res) => {
     Article.findAll({
         include:[{model:Category,required:true}]
@@ -57,6 +58,94 @@ router.post("/articles/delete", (req,res) => {
     else{
         res.redirect("/admin/articles");
     }
+})
+
+
+router.get("/admin/articles/edit/:id",(req,res)=>{
+    let id = req.params.id;
+    Article.findByPk(id).then(article=>{
+        if(article != undefined){
+            Category.findAll().then(categories=>{
+                res.render("admin/articles/edit",{
+                    categories:categories,
+                    article:article
+                })
+            })
+        }
+        else{
+            res.redirect("/")
+        }
+    }).catch(Erro =>{
+        res.redirect("/")
+    })
+
+})
+
+
+router.post("/articles/update",(req,res)=>{
+    let id = req.body.id;
+    let title = req.body.title;
+    let body = req.body.body;
+    let category = req.body.category;
+
+    Article.update({
+        title:title,
+        body: body,
+        categoryId: category,
+        slug:slugify(title),
+    },{
+        where:{
+            id:id
+        }
+    }).then(()=>{
+        res.redirect("/admin/articles");
+    }).catch(Erro=>{
+        res.redirect("/");
+    })
+})
+
+
+router.get("/articles/page/:num",(req,res)=>{ // PAGINAÇÃO
+    let page = req.params.num;
+    let offset = 0;
+
+    if(isNaN(page) || page == 1){
+        offset = 0;
+    }else{
+        offset = (parseInt(page)-1) * 4;
+    }
+
+    Article.findAndCountAll({
+        limit: 4,
+        offset: offset,
+        order:[
+            ['id','DESC']
+        ]
+    }).then(articles=>{
+
+        let next;
+        if(offset + 4 >= articles.count){
+            next = false;
+        }else{
+            next = true;
+        }
+
+        let result = {
+            page:parseInt(page),
+            next:next,
+            articles: articles
+        }
+
+
+        Category.findAll().then(categories=>{
+            res.render("admin/articles/page",{
+                result: result,
+                categories:categories
+            })
+        })
+
+    })
+
 })
 
 
--- index.js
+++ index.js
@@ -36,14 +36,74 @@ app.use("/",categoriesController);
 app.use("/",articlesController);
 
 
-//ROTAS
+
+
 app.get("/",(req,res) => {
-    Article.findAll().then(articles => {
-        res.render("index",{
-            articles:articles
-        });
+    Article.findAll({
+        order:[['id','DESC']],
+        limit: 4
+    }).then(articles => {
+        Category.findAll().then(categories =>{
+            res.render("index",{
+                articles:articles,
+                categories:categories
+            });
+        })
     })
 })
+
+
+
+// ARTIGO ESPECIFICO
+app.get("/:slug",(req,res) => {
+    let slug = req.params.slug;
+    Article.findOne({
+        where:{
+            slug: slug
+        }
+    }).then(article => {
+        if(article != undefined){
+            Category.findAll().then(categories =>{
+                res.render("article",{
+                    article: article,
+                    categories:categories
+                })
+            })
+        }
+        else{
+            res.redirect("/")
+        }
+    }).catch(erro => {
+        res.redirect("/")
+    })
+})
+
+
+app.get("/category/:slug",(req,res)=>{
+    let slug = req.params.slug;
+    Category.findOne({
+        where:{
+            slug:slug
+        },
+        include:[{model:Article}]
+    }).then(category =>{
+        if(category != undefined){
+            Category.findAll().then(categories=>{
+                res.render("index",{
+                    categories:categories,
+                    articles:category.articles
+                })
+            })
+        }
+        else{
+            res.redirect("/")
+        }
+    }).catch(Erro =>{
+        res.redirect("/")
+    })
+})
+
+
 
 app.listen(1010, () => {
     console.log("App rodando...")
