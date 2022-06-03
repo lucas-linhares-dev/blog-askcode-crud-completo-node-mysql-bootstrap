1255d61b6d683b0093e12ad288183165348db361
Lucas <lucas_linhares_@hotmail.com>
2022-06-03 00:15:02 -0300

    Commit dia 06, implementando funcionalidades do admin e front end

---
+++ articles/Article.js
@@ -0,0 +1,28 @@
+const Sequelize = require("sequelize");
+const connection = require("../database/database");
+const Category = require("../categories/Category");
+
+const Article = connection.define("articles",{
+
+    title:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    slug:{
+        type: Sequelize.STRING,
+        allowNull: false
+    },
+
+    body:{
+        type: Sequelize.STRING,
+        allowNull: false
+    }
+    
+})
+
+Article.belongsTo(Category);
+Category.hasMany(Article);
+
+
+module.exports = Article;
--- index.js
+++ index.js
@@ -3,6 +3,13 @@ const app = express();
 const bodyParser = require("body-parser");
 const connection = require("./database/database");
 
+const categoriesController = require("./categories/categoriesController");
+const articlesController = require("./articles/articlesController")
+
+const Article = require("./articles/Article");
+const Category = require("./categories/Category");
+
+
 //DATABASE CONNECTION
 connection
     .authenticate()
@@ -13,18 +20,29 @@ connection
         console.log(msgErro);
     })
 
+
 //EJS + PUBLIC
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
+
 
 //BODY-PARSER
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
 
+// CONTROLLERS
+app.use("/",categoriesController);
+app.use("/",articlesController);
+
+
 //ROTAS
 app.get("/",(req,res) => {
-    res.render("home");
+    Article.findAll().then(articles => {
+        res.render("index",{
+            articles:articles
+        });
+    })
 })
 
 app.listen(1010, () => {
